6c412d6024fdb09c49f5e83adf9fc269cd510658
Sudhanshu Shekhar <91026313+sud516@users.noreply.github.com>
2022-02-28 19:08:34 +0530

    v1

--- js.js
+++ js.js
@@ -1,16 +1,35 @@
-for (let i = 0; i < 3000; ++i) {
+let x = screen.height;
+let y = screen.width;
+let totalcon = x * y;
+console.log(totalcon);
+let mycon = 20 * 21;
+console.log(mycon);
+let z = totalcon / mycon;
+console.log(z);
+
+for (let i = 1; i <= z; ++i) {
   const box = document.createElement("div");
   box.classList.add("mybox");
-  box.addEventListener("mouseenter", () => {
-    box.style.backgroundColor = "red";
+
+  box.addEventListener("mousemove", () => {
+    box.style.backgroundColor = "yellow";
   });
-  box.addEventListener("mouseleave", () => {
+  box.addEventListener("touchmove", () => {
+    box.style.backgroundColor = "yellow";
+  });
+  box.addEventListener("touchend", () => {
     box.style.backgroundColor = "black";
-    box.style.transition = "background-color 3s";
+    box.style.transition = "background-color 0.5s";
   });
+
+  box.addEventListener("mouseleave", (e) => {
+    box.style.backgroundColor = "black";
+    box.style.transition = "background-color 0.5s";
+  });
+
   document.body.appendChild(box);
 }
-const span = document.createElement("span");
-span.innerText = "    PIXEL ERROR 🤐🤐🤐";
-span.style.color = "black";
-document.body.appendChild(span);
+// const span = document.createElement("span");
+// span.innerText = "    PIXEL ERROR 🤐🤐🤐";
+// span.style.color = "black";
+// document.body.appendChild(span);
